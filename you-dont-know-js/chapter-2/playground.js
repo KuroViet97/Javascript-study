fd2e2b287e9da58afb16c08615b49f45115169e3
viet.nguyen <viet.nguyen@ubitec.com>
2020-03-04 16:44:58 +0700

    Javascript: add playground, study notes

---
+++ you-dont-know-js/chapter-2/playground.js
@@ -0,0 +1,18 @@
+var myObject = {
+      first: "Viet",
+      last: "Nguyen",
+      age: 18,
+      single: false,
+      jobs: ["student", "software engineer", "porn lover"]
+}
+
+//console.log(myObject.jobs.length);
+//console.log(`My name is: ${ myObject.first }`);
+//Here are some examples about typeof:
+
+console.log(typeof myObject.first);
+console.log(typeof myObject.age);
+console.log(typeof myObject.single);
+console.log(typeof myObject.jobs); //array
+console.log(typeof myObject); //object
+console.log(typeof null);
